e7aed13759ca0402f06b8f99ff3a04f29cb87a0c
Anri Digholm <anridig@gmail.com>
2023-03-18 13:10:04 +0900

    Typos.

--- cmd/ipcheck/ipcheck.go
+++ cmd/ipcheck/ipcheck.go
@@ -12,7 +12,7 @@ func main() {
 	inputFileOrURL := pflag.StringP("input-file", "i", "", "Path or URL to an input file containing IP addresses to check. This can be an uncompressed text file in any format. The program finds all IPs addresses on each line and tests them against all ranges.")
 	ipRangesFileOrURL := pflag.String("ip-ranges", "https://raw.githubusercontent.com/jhassine/server-ip-addresses/master/data/datacenters.csv", "Path or URL to a CSV file with IP ranges to test against.")
 	downloadFireHOLTo := pflag.String("download", "", "Download all blocklists from FileHOL repo (https://github.com/firehol/blocklist-ipsets) and merge them into one big file named `firehol.ips` in this dir")
-	forceDownloadFireHOL := pflag.Bool("force-download", false, "Force (re)download of FileHOL blocklist (will delete locally cache files)")
+	forceDownloadFireHOL := pflag.Bool("force-download", false, "Force (re)download of all FileHOL blocklists (will delete locally cached files)")
 	fireHOLFile := pflag.StringP("firehol-file", "f", "", "Import all IP sets from https://github.com/firehol/blocklist-ipsets, merge them into one CSV file in this dir")
 	verbose := pflag.Bool("verbose", false, "Verbose output, helps when troubleshooting.")
 
